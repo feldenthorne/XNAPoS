9433eb8d4b479eef53516ec641ec09d232d697bb
Robert Zimmerman <feldenthorne@users.sf.net>
2015-05-09 10:21:55 -0700

    Bumping client version (2.2.2.0)
    Because PoS is fixed, its a good time to bump the client and
    RPC protocol version. Once a majority of users have downloaded
    and synced the proper blockchain with this client version, this
    new protocol version should become the connection cutoff.

--- src/clientversion.h
+++ src/clientversion.h
@@ -7,12 +7,12 @@
 
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2
-#define CLIENT_VERSION_MINOR       1
+#define CLIENT_VERSION_MINOR       2
 #define CLIENT_VERSION_REVISION    2
 #define CLIENT_VERSION_BUILD       0
 
 // This is the client version name, used by the GUI and server for version reporting
-#define CLIENT_VERSION_NAME "Adenine"
+#define CLIENT_VERSION_NAME "Thymine"
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
